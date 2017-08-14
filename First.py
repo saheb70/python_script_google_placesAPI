53e79abbc827a031ff740180ed70f47c6e5c0626
saheb70 <sahebdua70@gmail.com>
2017-08-14 17:21:29 +0530

    Update First.py

--- First.py
+++ First.py
@@ -1,3 +1,5 @@
+#to find places on google maps,firstly place id is needed.
+# in this First.py it will fetch place id's of locations and store it in Place_ID.csv
 import urllib2
 import json
 import csv
@@ -9,8 +11,8 @@ sys.setdefaultencoding('utf8')
 f=open('Plcace_ID.csv','w')
 writer=csv.writer(f)
 
-query=''#it takes search input eg. 'cafe+in+delhi'
-API_key=''#Insert your google places key here
+query=''   #it takes search input eg. 'cafe+in+delhi'
+API_key='' #Insert your google places key here
 
 
 url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' +query+ '&region=in&key='+API_key+''
@@ -25,11 +27,10 @@ while True:
     for i in results:
         placeid = i.get("place_id")
         name = i.get("name")
-        #remove '#' to see current search while runnig 
 	#print placeid, name
         writer.writerow([name,placeid])
     time.sleep(2)
     response = urllib2.urlopen('https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + query + '&region=in&key='+API_key+'&pagetoken=' + str(page_token) + '')	
-    if page_token==None:
-	    break
+    if page_token==None:# google only gives 20 output per page and max pages can be 3. to get next page page_token is given on each page
+	    break#if we do not get page_token so it means we are at the last page and code will break.
     
